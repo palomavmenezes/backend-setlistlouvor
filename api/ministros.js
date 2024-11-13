80ddfdf9502257595cb98b7cc2cd3408a7dbd163
palomavmenezes <palomacg.contato@gmail.com>
2024-11-13 19:17:13 -0300

    Mudando estrutura para vercel

--- api/ministros.js
+++ api/ministros.js
@@ -3,19 +3,29 @@ const { Pool } = require('pg');
 
 // Cria uma instância do Pool com variáveis de ambiente
 const pool = new Pool({
-  user: process.env.DB_USER,
   host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT || 5432,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM ministros');
-    res.status(200).json(result.rows);
-  } catch (error) {
-    console.error('Erro ao consultar o banco de dados:', error);
-    res.status(500).json({ error: 'Erro ao buscar dados do banco de dados.' });
+  if (req.method === 'GET') {
+    try {
+      const result = await pool.query('SELECT * FROM ministros');
+      res.status(200).json({ data: result.rows });
+    } catch (err) {
+      console.error('Erro na consulta ao banco de dados', err);
+      res.status(500).json({
+        message: "Erro interno do servidor. Não foi possível listar os ministros.",
+        error: err.message
+      });
+    }
+  } else {
+    res.status(405).json({ message: 'Método não permitido.' });
   }
 };
