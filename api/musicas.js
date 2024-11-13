c4cf0106f1697b562e71ac30fb0dd8beb050ba63
palomavmenezes <palomacg.contato@gmail.com>
2024-11-13 19:36:11 -0300

    Subindo arquivos com json de musicas

--- api/musicas.js
+++ api/musicas.js
@@ -1,15 +1,15 @@
-// api/ministros.js
+// api/musicas.js
 const pool = require('../db');
 
 module.exports = async (req, res) => {
   if (req.method === 'GET') {
     try {
-      const result = await pool.query('SELECT * FROM ministros');
+      const result = await pool.query('SELECT * FROM musicas');
       res.status(200).json({ data: result.rows });
     } catch (err) {
       console.error('Erro na consulta ao banco de dados', err);
       res.status(500).json({
-        message: "Erro interno do servidor. Não foi possível listar os ministros.",
+        message: "Erro interno do servidor. Não foi possível listar as músicas.",
         error: err.message,
       });
     }
